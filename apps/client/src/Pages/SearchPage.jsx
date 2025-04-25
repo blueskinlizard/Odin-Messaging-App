5adab68a816bf26250631f07a5eab7ccae14a5f7
blueskinlizard <blueskinlizard@gmail.com>
2025-04-25 06:17:25 -0500

    Search page component debugged

--- apps/client/src/Pages/SearchPage.jsx
+++ apps/client/src/Pages/SearchPage.jsx
@@ -1,12 +1,29 @@
 import { useState, useEffect } from "react"
 import ProfileDisplay from "../Components/ProfileDisplay"
 export default function SearchPage(){
-    const [searchTerm, setSearchTerm] = useState(null);
+    const [searchTerm, setSearchTerm] = useState("");
     const [searchResults, setSearchResults] = useState(null);
 
     useEffect(()=>{
+        const handleSearch = async() =>{ 
+            //Whenever searchterm changes in dependency array, ths is reran, searching again
+            try{
+                const fetchedSearchDataJson = await fetch('http://localhost:8080/api/findUser', {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ userName: searchTerm.toLowerCase()})
+                });
+                const fetchedSearchData = await fetchedSearchDataJson.json();
+                setSearchResults(fetchedSearchData); 
+            }catch(err){
+                console.error("Error fetching search data", err);
+            }
+            //Set state to entire fetchedSearchData, so we can pass down value from here instead of doing ANOTHER fetch
+        }
         if(searchTerm){
-            handleSearch
+            handleSearch();
         }
     }, [searchTerm])
     const handleForm = (e) => {
@@ -14,23 +31,7 @@ export default function SearchPage(){
         setSearchTerm(e.target.userSearchInput.value);
     }
 
-    const handleSearch = async() =>{
-        try{
-            const fetchedSearchDataJson = await fetch('http://localhost:8080/api/findUser', {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ userName: searchTerm.toLowerCase()})
-            });
-            const fetchedSearchData = await fetchedSearchDataJson.json();
-            setSearchResults(fetchedSearchData); 
-        }catch{
-            console.error("Error fetching search data");
-        }
-        //Set state to entire fetchedSearchData, so we can pass down value from here instead of doing ANOTHER fetch
-    }
-    handleSearch();
+    
     return(
         <div className="SearchPage">
             <form id="searchForm" onSubmit={handleForm}>
@@ -40,9 +41,11 @@ export default function SearchPage(){
                 searchResults?.name === "User not found" ? (
                     <h1>User not found</h1>
                 ) : (
-                    <div className="userReturnedData">
+                    searchResults && (
+                     <div className="userReturnedData">
                         <ProfileDisplay user={searchResults} />
-                    </div>
+                     </div>
+                    )
                 )
             }
         </div>
